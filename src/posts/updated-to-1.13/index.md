73fcb1efe277b9c2de2776815df929637768ab1a
Oleksii Chekulaiev <achekulaev@gmail.com>
2019-11-19 19:24:03 -0500

    Change default layout for blog post

--- src/posts/updated-to-1.13/index.md
+++ src/posts/updated-to-1.13/index.md
@@ -4,13 +4,14 @@ date: "2019-11-19T22:00:00.000Z"
 path: "/updated-to-1.13/"
 ---
 
-# You have updated Docksal to 1.13 
+# You have updated Docksal to 1.13! 
 
-Exciting! Let's see what's new!
+Exciting times! Let's see what's new.
 
 ## macOS
 
-- NFS is now default volumes
+- NFS is now default for `DOCKSAL_VOLUMES`
+- But we forcibly set it to `bind` during update so that nothing break
 
 ## windows
 
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import NavSubpages from '../components/NavSubpages'
 import Link from 'gatsby-link'
 import get from 'lodash/get'
 
 class BlogPostTemplate extends React.Component {
   render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+    const post = this.props.data.markdownRemark;
+    const siteTitle = get(this.props, 'data.site.siteMetadata.title');
 
     return (
-      <div>
+      <div id={`blog-post`}>
+        <div>&nbsp;</div>
         <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} />
+        <NavSubpages sticky={false} />
+
         <h1>{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <div id={`main`} dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     )
   }
